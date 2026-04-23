381f1f8163cb87532b1ee66300eed68f4e159f64
Daniel DiCesare <dicesard@email.uscb.edu>
2026-04-23 14:01:54 -0400

    revert back to old version

---
+++ FinalProject4-15.py
@@ -0,0 +1,12 @@
+# Developer:    Daniel, DiCesare
+# Course:       B104
+# Assignment:   Final Project
+
+import pandas as pd
+
+df = pd.read_excel("B104_Project_Data2.xlsx")
+
+print(df.head())
+
+# To do: convert stuff like an answer of 1.0 for question 1 
+# to answer A of 12 years old
